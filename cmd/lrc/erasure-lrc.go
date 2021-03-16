b15ce86866203655cd482e27efb40075e71206f9
hittrickcr7 <lnsydengyuang@163.com>
2021-03-16 09:30:01 +0800

    dya-lrc

--- cmd/lrc/erasure-lrc.go
+++ cmd/lrc/erasure-lrc.go
@@ -268,9 +268,7 @@ func (l *lrc)reconstruct(shards [][]byte, dataOnly bool) error {
 		return err
 	}
 	for k,v :=range group1Present{
-		if k {
 
-		}
 	}
 	if len(group1Present)==2 {
 		reconstructGroup()
